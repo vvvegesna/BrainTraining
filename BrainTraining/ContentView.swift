38bd4814f18f0f3cccae96b2fb7f03fe98c23fd9
Vegesna, Vijay V EX1 <VVegesna@us.imshealth.com>
2020-02-10 16:50:27 -0700

    Initial Commit

---
+++ BrainTraining/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrainTraining
+//
+//  Created by Vegesna, Vijay V EX1 on 2/10/20.
+//  Copyright © 2020 Vegesna, Vijay V EX1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
